b66849bc072d50dc752e2463a7b2558d46c93022
Sasha Xox <sasha_xox@tut.by>
2024-07-01 17:07:38 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import { useEffect } from "react";
-import "./App.css";
-
-const tg = window.Telegram.WebApp;
+import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 function App() {
-  useEffect(() => {
-    tg.ready()
-  }, []);
+    const {onToggleButton, tg} = useTelegram();
 
-  const onClose = () => {
-    tg.close();
-  };
+    useEffect(() => {
+        tg.ready();
+    }, [])
 
-  return (
-    <div className="App">
-      <button onClick={onClose}>закрыть</button>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
